965814a13e6dd1d7ff83ec46719976f55a5e0649
danielaYz2207 <dan.yaneza@duocuc.cl>
2023-07-13 04:54:08 -0400

    Se pulen las páginas de home menu nosotros y el footer

--- src/app/components/vectorMenu/index.jsx
+++ src/app/components/vectorMenu/index.jsx
@@ -1,16 +1,10 @@
 import React from "react";
 import Style from "./vectormenu.module.css";
-import Image from "next/image";
 
 export default function VectorMenu() {
   return (
     <div className={Style.vectorContenedor}>
-      <Image
-        src={"/image/lineas_divisoras.svg"}
-        alt="Lineas"
-        width={1440}
-        height={40}
-      />
+      <img src="/image/lineas_divisoras.svg"></img>
     </div>
   );
 }
